42bc6becc19b3838eb4342ccd0ff58697288f1f1
ryanongwx <ryanongwx@gmail.com>
2023-01-16 00:46:29 +0800

    Basic Functionalities done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:bio, :image_id, :password)
+      params.require(:user).permit(:bio, :image_id, :password, :token, :username)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_07_095853) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_12_083838) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_07_095853) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "token"
   end
 
   add_foreign_key "comments", "posts"
