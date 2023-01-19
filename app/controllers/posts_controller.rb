090d77bb641a200100776e0b5897dc4461791e4f
ryanongwx <ryanongwx@gmail.com>
2023-01-19 12:26:07 +0800

    Added category to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:user_id, :title, :body, :recommend, :unrecommend)
+      params.require(:post).permit(:user_id, :title, :body, :recommend, :unrecommend, :category)
     end
 end
